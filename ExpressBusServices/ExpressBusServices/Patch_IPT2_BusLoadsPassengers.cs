8218ca4bca1c6b2c4729af90c63d580b57136a46
Vectorial1024 <vincentwwy168@gmail.com>
2020-10-21 10:55:09 +0800

    Code cleanup

--- ExpressBusServices/ExpressBusServices/Patch_IPT2_BusLoadsPassengers.cs
+++ ExpressBusServices/ExpressBusServices/Patch_IPT2_BusLoadsPassengers.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using HarmonyLib;
 using System.Reflection;
-using System.Text;
-using HarmonyLib;
 
 namespace ExpressBusServices
 {

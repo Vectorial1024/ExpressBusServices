30121b8d701564fd815993c2023ccb8901566627
Vectorial1024 <vincentwwy168@gmail.com>
2020-10-25 15:26:01 +0800

    Rename file tp reflect generality

--- ExpressBusServices/ExpressBusServices/Patch_IPT2_TransportLoadsPassengers.cs
+++ ExpressBusServices/ExpressBusServices/Patch_IPT2_TransportLoadsPassengers.cs
@@ -5,7 +5,7 @@ namespace ExpressBusServices
 {
     [HarmonyPatch]
     [HarmonyPriority(Priority.High)]
-    public class Patch_IPT2_BusLoadsPassengers
+    public class Patch_IPT2_TransportLoadsPassengers
     {
         [HarmonyTargetMethod]
         public static MethodBase TargetRelevantIPT2Method()
@@ -23,6 +23,7 @@ namespace ExpressBusServices
         public static void HandleBusArrivedAtTarget(ushort vehicleID)
         {
             // thanks to IPT, I have to do things in a very roundabout way.
+            // but, in another perspective, this covers all the transport types that IPT can cover
             Vehicle data = VehicleManager.instance.m_vehicles.m_buffer[vehicleID];
             BusPickDropLookupTable.Notify_PassengersBoardedOntoBus(vehicleID, data.m_transferSize);
         }

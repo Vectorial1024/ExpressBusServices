46ef50a2112296196e74add374c412c86a4ed36a
Vectorial1024 <vincentwwy168@gmail.com>
2021-05-25 01:11:14 +0800

    Clarify semantics of bus departure checker
    It caused me to shoot myself in the foot during development...

--- ExpressBusServices/ExpressBusServices/DepartureChecker.cs
+++ ExpressBusServices/ExpressBusServices/DepartureChecker.cs
@@ -13,8 +13,9 @@ namespace ExpressBusServices
         {
             TransportManager instance = Singleton<TransportManager>.instance;
             ushort transportLineID = vehicleData.m_transportLine;
-            ushort firstStop = instance.m_lines.m_buffer[transportLineID].GetStop(1);
-            ushort currentStop = vehicleData.m_targetBuilding;
+            // the semantics! they must be clear! dont try to cheat with "first is index=1 and current=next" again! it hurts devops!
+            ushort firstStop = instance.m_lines.m_buffer[transportLineID].GetStop(0);
+            ushort currentStop = TransportLine.GetPrevStop(vehicleData.m_targetBuilding);
 
             // note:
             // if I somehow cannot determine where I am at or where the first stop is at, always unbunch.

2f19fafd3cfce063201dec7f59b593272537264f
Vectorial1024 <vincentwwy168@gmail.com>
2022-03-13 22:31:11 +0800

    Create ModDetector.cs

---
+++ ExpressBusServices/ExpressBusServices/ModDetector.cs
@@ -0,0 +1,12 @@
+﻿using HarmonyLib;
+
+namespace ExpressBusServices
+{
+    internal class ModDetector
+    {
+        public static bool TransportLinesManagerIsLoaded()
+        {
+            return AccessTools.TypeByName("Klyte.TransportLinesManager.TLMController")?.GetMethod("AutoColor") != null;
+        }
+    }
+}

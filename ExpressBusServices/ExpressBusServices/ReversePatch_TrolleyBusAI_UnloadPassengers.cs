1da887239eeb68da02f6fd7a25972f6dab7db9cb
Vectorial1024 <vincentwwy168@gmail.com>
2022-03-13 22:46:51 +0800

    Reverse patches for TrolleybusAI

---
+++ ExpressBusServices/ExpressBusServices/ReversePatch_TrolleyBusAI_UnloadPassengers.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using System;
+
+namespace ExpressBusServices
+{
+    [HarmonyPatch(typeof(TrolleybusAI))]
+    [HarmonyPatch("UnloadPassengers", MethodType.Normal)]
+    public class ReversePatch_TrolleybusAI_UnloadPassengers
+    {
+        [HarmonyReversePatch]
+        public static bool TrolleybusAI_UnloadPassengers(object __instance, ushort vehicleID, ref Vehicle vehicleData, ushort currentStop, ushort nextStop)
+        {
+            throw new NotImplementedException("This is a stub that is not available at this moment.");
+        }
+    }
+}
---
+++ ExpressBusServices/ExpressBusServices/ReversePatch_TrolleybusAI_StartPathFind.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using System;
+
+namespace ExpressBusServices
+{
+    [HarmonyPatch(typeof(TrolleybusAI))]
+    [HarmonyPatch("StartPathFind", MethodType.Normal)]
+    public class ReversePatch_TrolleybusAI_StartPathFind
+    {
+        [HarmonyReversePatch]
+        public static bool TrolleybusAI_StartPathFind(object __instance, ushort vehicleID, ref Vehicle vehicleData)
+        {
+            throw new NotImplementedException("This is a stub that is not available at this moment.");
+        }
+    }
+}

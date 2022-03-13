30c5f678f4879c5e00617dbf4bbacf6ffff2c55b
Vectorial1024 <vincentwwy168@gmail.com>
2022-03-13 22:36:26 +0800

    Create ReversePatch_BusAI_StartPathFind.cs

---
+++ ExpressBusServices/ExpressBusServices/ReversePatch_BusAI_StartPathFind.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using System;
+
+namespace ExpressBusServices
+{
+    [HarmonyPatch(typeof(BusAI))]
+    [HarmonyPatch("StartPathFind", MethodType.Normal)]
+    public class ReversePatch_BusAI_StartPathFind
+    {
+        [HarmonyReversePatch]
+        public static bool BusAI_StartPathFind(object __instance, ushort vehicleID, ref Vehicle vehicleData)
+        {
+            throw new NotImplementedException("This is a stub that is not available at this moment.");
+        }
+    }
+}

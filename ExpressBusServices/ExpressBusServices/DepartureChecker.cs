f4b5a66c45863bab90fd3133e71f9d09e6030e7e
Vectorial1024 <vincentwwy168@gmail.com>
2022-03-21 15:13:00 +0800

    Fix checking wrong stop as 1st stop

--- ExpressBusServices/ExpressBusServices/DepartureChecker.cs
+++ ExpressBusServices/ExpressBusServices/DepartureChecker.cs
@@ -14,7 +14,7 @@ namespace ExpressBusServices
             TransportManager instance = Singleton<TransportManager>.instance;
             ushort transportLineID = vehicleData.m_transportLine;
             // the semantics! they must be clear! dont try to cheat with "first is index=1 and current=next" again! it hurts devops!
-            ushort firstStop = instance.m_lines.m_buffer[transportLineID].GetStop(0);
+            ushort firstStop = instance.m_lines.m_buffer[transportLineID].GetStop(1);
             ushort currentStop = TransportLine.GetPrevStop(vehicleData.m_targetBuilding);
 
             // note:

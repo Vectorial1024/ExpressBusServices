3c74f80d906089d55a9712e9ad1ff019c9c02865
Vincent Wong <vincentwwy168@gmail.com>
2022-07-23 17:39:41 +0800

    Create function to analyze transport line popularity

--- ExpressBusServices/ExpressBusServices/ServiceBalancerUtil.cs
+++ ExpressBusServices/ExpressBusServices/ServiceBalancerUtil.cs
@@ -35,9 +35,67 @@ namespace ExpressBusServices
             return false;
         }
 
-        private static void AnalyzeTransportLinePopularity()
+        private static List<TransportLineSegmentAnalysis> AnalyzeTransportLinePopularity(ushort transportLineID, ushort startingTerminusStopId)
         {
             // checks segment terminus -> segment total waiting pax
+            // the first item of the list is guaranteed to be the "current segment"
+            ushort loopingStopID = startingTerminusStopId;
+            Dictionary<ushort, int> paxCount = new Dictionary<ushort, int>();
+            Dictionary<ushort, bool> terminusCheck = new Dictionary<ushort, bool>();
+            Dictionary<ushort, ushort> nextStopLink = new Dictionary<ushort, ushort>();
+            while (true)
+            {
+                if (paxCount.ContainsKey(loopingStopID))
+                {
+                    // we looped back to the start
+                    break;
+                }
+
+                // check waiting passengers
+                int residentsWaiting, touristsWaiting;
+                TransportLineUtil.CountPassengersWaiting(loopingStopID, out residentsWaiting, out touristsWaiting);
+                paxCount.Add(loopingStopID, residentsWaiting + touristsWaiting);
+
+                // check is terminus
+                bool isTerminus = DepartureChecker.StopIsConsideredAsTerminus(loopingStopID, transportLineID);
+                if (isTerminus)
+                {
+                    terminusCheck.Add(loopingStopID, isTerminus);
+                }
+
+                // next stop
+                ushort nextStopId = TransportLine.GetNextStop(loopingStopID);
+                nextStopLink.Add(loopingStopID, nextStopId);
+                loopingStopID = nextStopId;
+            }
+
+            // all information obtained; we are at the first stop of the line
+            // create the list
+            List<TransportLineSegmentAnalysis> analysisList = new List<TransportLineSegmentAnalysis>();
+            TransportLineSegmentAnalysis analysis = new TransportLineSegmentAnalysis(startingTerminusStopId, 1, paxCount[startingTerminusStopId], paxCount[startingTerminusStopId] > 30);
+            loopingStopID = nextStopLink[startingTerminusStopId];
+            while (true)
+            {
+                if (terminusCheck.ContainsKey(loopingStopID))
+                {
+                    analysisList.Add(analysis);
+                    analysis = new TransportLineSegmentAnalysis(startingTerminusStopId, 0, 0, false);
+                }
+                if (loopingStopID == startingTerminusStopId)
+                {
+                    // we got to the start again
+                    break;
+                }
+                // add info
+                analysis.stopCount++;
+                analysis.paxCount += paxCount[loopingStopID];
+                analysis.segmentCanReceiveRedeployment |= paxCount[loopingStopID] > 30;
+                // move to next
+                loopingStopID = nextStopLink[startingTerminusStopId];
+            }
+
+            // return the list
+            return analysisList;
         }
 
         private static bool OddsPermitRedeployment()

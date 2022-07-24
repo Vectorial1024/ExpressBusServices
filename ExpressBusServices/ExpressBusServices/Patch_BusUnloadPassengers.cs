fc7f917d0a9abd45ed22d0fab6d2ac991074476f
Vincent Wong <vincentwwy168@gmail.com>
2022-07-24 17:29:33 +0800

    Tell the game where each vehicle is at

--- ExpressBusServices/ExpressBusServices/Patch_BusUnloadPassengers.cs
+++ ExpressBusServices/ExpressBusServices/Patch_BusUnloadPassengers.cs
@@ -13,6 +13,7 @@ namespace ExpressBusServices
             ushort transportLineId = data.m_transportLine;
             // not yet target the next stop
             ushort currentTerminusStopId = data.m_targetBuilding;
+            ServiceBalancerUtil.MarkVehicleIsAtStopId(vehicleID, currentTerminusStopId);
             if (ServiceBalancerUtil.FindRedeployToTerminus(transportLineId, currentTerminusStopId, out redeploymentTarget))
             {
                 // set destination to somewhere else

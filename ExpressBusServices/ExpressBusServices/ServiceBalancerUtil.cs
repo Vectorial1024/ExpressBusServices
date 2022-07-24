699fa5c493c45c32b1f3eb5500f4e1dceda8058d
Vincent Wong <vincentwwy168@gmail.com>
2022-07-24 19:14:54 +0800

    Increase probability

--- ExpressBusServices/ExpressBusServices/ServiceBalancerUtil.cs
+++ ExpressBusServices/ExpressBusServices/ServiceBalancerUtil.cs
@@ -204,7 +204,7 @@ namespace ExpressBusServices
             }
             // finally, do a RNG with such probability * the global config prob value
             // todo read from a config, or not
-            float globalBalancerProbability = 0.3f;
+            float globalBalancerProbability = 0.35f;
             float theProbability = probability * globalBalancerProbability;
             // Random.value gives a PseudoUniform(0, 1) random value
             float rngPick = UnityEngine.Random.value;

ab5ffdd550434055012b608416197c4d18c9ed69
Vincent Wong <vincentwwy168@gmail.com>
2022-08-13 19:53:22 +0800

    Connect the logic to the code

---
+++ ExpressBusServices/ExpressBusServices/Patch_TramAiCorrector.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace ExpressBusServices
+{
+    [HarmonyPatch(typeof(TramAI))]
+    [HarmonyPatch("CanLeave", MethodType.Normal)]
+    public class Patch_TramAiCorrector
+    {
+        [HarmonyPostfix]
+        public static void PostFix(ref bool __result, ushort vehicleID, ref Vehicle vehicleData)
+        {
+            TramPickDropLookupTable.DetermineIfTramShouldDepart(ref __result, vehicleID, ref vehicleData);
+        }
+    }
+}

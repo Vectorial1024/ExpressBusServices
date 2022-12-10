5fd7d2bb670348e1a38a8b60c1c7a9b25e1b51f4
Vincent Wong <vincentwwy168@gmail.com>
2022-12-10 14:35:53 +0800

    New vehicles in line now checks for teleport-redeploy

---
+++ ExpressBusServices/ExpressBusServices/Patch_TransportLine_AddVehicle.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace ExpressBusServices
+{
+    [HarmonyPatch(typeof(TransportLine))]
+    [HarmonyPatch("AddVehicle", MethodType.Normal)]
+    public class Patch_TransportLine_AddVehicle
+    {
+        [HarmonyPostfix]
+        public static void PostFix(ushort vehicleID, ref Vehicle data, bool findTargetStop)
+        {
+            if (!findTargetStop)
+            {
+                // we are loading from save; DO NOT modify the vehicle details!
+                return;
+            }
+            ushort transportLineID = data.m_transportLine;
+            if (transportLineID == 0)
+            {
+                return;
+            }
+            if (BusDepotRedeploymentInstructions.TransportLineReadFutureDeployment(transportLineID, out ushort targetStopID))
+            {
+                data.m_targetBuilding = targetStopID;
+                Debug.Log($"New vehicle of transport line ${transportLineID} now redeploying to stop {targetStopID} as per future instructions.");
+            }
+        }
+    }
+}

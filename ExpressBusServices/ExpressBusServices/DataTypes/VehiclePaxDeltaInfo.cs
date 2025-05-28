ca8f76101dd8ab832427846d7a98e0d6885c2d9e
Vincent Wong <vincentwwy168@gmail.com>
2025-05-28 22:32:11 +0800

    Implement advanced accessors for pax-delta info

--- ExpressBusServices/ExpressBusServices/DataTypes/VehiclePaxDeltaInfo.cs
+++ ExpressBusServices/ExpressBusServices/DataTypes/VehiclePaxDeltaInfo.cs
@@ -4,24 +4,24 @@ using System.Collections.Generic;
 namespace ExpressBusServices.DataTypes
 {
     /// <summary>
-    /// A struct for remembering per-vehicle passenger delta information, assuming the vehicle is a local public transport vehicle now stopped at some stop.
+    /// A class for remembering per-vehicle passenger delta information, assuming the vehicle is a local public transport vehicle now stopped at some stop.
     /// <para/>
     /// It just turns out, all kinds of public transport vehicles share essentially the same flow. 
     /// </summary>
-    public struct VehiclePaxDeltaInfo
+    public class VehiclePaxDeltaInfo
     {
         public int PaxAlighted { get; set; }
-        
+
         public int PaxBeforeBoarding { get; set; }
-        
+
         public int PaxAfterBoarding { get; set; }
-        
+
         public int PaxActualBoarded => PaxAfterBoarding - PaxBeforeBoarding;
-        
+
         public bool PaxHasDelta => PaxAlighted > 0 || PaxActualBoarded > 0;
-        
+
         private static Dictionary<ushort, VehiclePaxDeltaInfo> paxDeltaTable;
-        
+
         public static void TouchAndResetTable()
         {
             if (paxDeltaTable == null)
@@ -29,9 +29,9 @@ namespace ExpressBusServices.DataTypes
                 paxDeltaTable = new Dictionary<ushort, VehiclePaxDeltaInfo>();
             }
         }
-        
+
         public static void WipeTable() => paxDeltaTable?.Clear();
-        
+
         /// <summary>
         /// Resets the pax-delta info related to a vehicle, or touches it if it is not used before, assuming the vehicle is a local supported public transport vehicle.
         /// <para/>
@@ -42,7 +42,7 @@ namespace ExpressBusServices.DataTypes
         {
             paxDeltaTable[vehicleID] = new VehiclePaxDeltaInfo();
         }
-        
+
         /// <summary>
         /// Returns the pax-delta info of a vehicle. This does NOT consider the trailers of the vehicle.
         /// </summary>
@@ -63,5 +63,20 @@ namespace ExpressBusServices.DataTypes
         /// </summary>
         /// <param name="vehicleID">The ID of the vehicle in question.</param>
         public static void Remove(ushort vehicleID) => paxDeltaTable.Remove(vehicleID);
+
+        public static void Notify_VehicleHasUnloadedPax(ushort vehicleID, ref int serviceCounter)
+        {
+            Get(vehicleID).PaxAlighted = serviceCounter;
+        }
+
+        public static void Notify_VehicleStartsLoadingPax(ushort vehicleID, ref Vehicle data)
+        {
+            Get(vehicleID).PaxBeforeBoarding = data.m_transferSize;
+        }
+
+        public static void Notify_VehicleHasLoadedPax(ushort vehicleID, ref Vehicle data)
+        {
+            Get(vehicleID).PaxAfterBoarding = data.m_transferSize;
+        }
     }
 }

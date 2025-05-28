0a5cea91da240ec258d006387f20a6dd7c86463d
Vincent Wong <vincentwwy168@gmail.com>
2025-05-29 00:12:32 +0800

    Create VehicleDepartureUtil

---
+++ ExpressBusServices/ExpressBusServices/Util/VehicleDepartureUtil.cs
@@ -0,0 +1,10 @@
+namespace ExpressBusServices.Util
+{
+    /// <summary>
+    /// The util class for handling customized vehicle departure logic
+    /// </summary>
+    public static class VehicleDepartureUtil
+    {
+        // wip
+    }
+}

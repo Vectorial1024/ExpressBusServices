36052871314802c4f8f2164d59d661cbcf0d3607
Vincent Wong <vincentwwy168@gmail.com>
2025-05-29 01:00:42 +0800

    Create stub method

--- ExpressBusServices/ExpressBusServices/Util/VehicleDepartureUtil.cs
+++ ExpressBusServices/ExpressBusServices/Util/VehicleDepartureUtil.cs
@@ -5,6 +5,17 @@ namespace ExpressBusServices.Util
     /// </summary>
     public static class VehicleDepartureUtil
     {
+        /// <summary>
+        /// The centralized method for reviewing whether the given local public transport vehicle should change its departure status.
+        /// This mod may hold or release vehicles according to a list of criteria, essentially overriding vanilla logic.
+        /// </summary>
+        /// <param name="__result">The result of this operation; if true, signals the game engine to depart the vehicle at the next sim-step.</param>
+        /// <param name="vehicleID">The ID of the vehicle in question.</param>
+        /// <param name="vehicleData">The data of the vehicle in question.</param>
+        public static void ReviewDepartureStatus(ref bool __result, ushort vehicleID, ref Vehicle vehicleData)
+        {
+            // wip
+        }
         // wip
     }
 }

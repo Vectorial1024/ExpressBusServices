bbfbb10e6b68d374b4270da812d1c7d5223e5067
Vincent Wong <vincentwwy168@gmail.com>
2025-05-30 19:33:11 +0800

    Switch trams to use the new unbunching logic

--- ExpressBusServices/ExpressBusServices/Patch_TramAiCorrector.cs
+++ ExpressBusServices/ExpressBusServices/Patch_TramAiCorrector.cs
@@ -1,16 +1,18 @@
-﻿using HarmonyLib;
+﻿using ExpressBusServices.Util;
+using HarmonyLib;
 using UnityEngine;
 
 namespace ExpressBusServices
 {
     [HarmonyPatch(typeof(TramAI))]
-    [HarmonyPatch("CanLeave", MethodType.Normal)]
+    [HarmonyPatch(nameof(TramAI.CanLeave), MethodType.Normal)]
     public class Patch_TramAiCorrector
     {
         [HarmonyPostfix]
         public static void PostFix(ref bool __result, ushort vehicleID, ref Vehicle vehicleData)
         {
-            TramPickDropLookupTable.DetermineIfTramShouldDepart(ref __result, vehicleID, ref vehicleData);
+            VehicleDepartureUtil.ReviewDepartureStatus(ref __result, vehicleID, ref vehicleData);
+            // TramPickDropLookupTable.DetermineIfTramShouldDepart(ref __result, vehicleID, ref vehicleData);
         }
     }
 }

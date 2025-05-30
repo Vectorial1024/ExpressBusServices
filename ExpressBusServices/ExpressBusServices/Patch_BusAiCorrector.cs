101bf5b2fcf5fdb54813b8c4b2ded162651a2d07
Vincent Wong <vincentwwy168@gmail.com>
2025-05-30 19:06:21 +0800

    Switch buses to use the new unbunching logic

--- ExpressBusServices/ExpressBusServices/Patch_BusAiCorrector.cs
+++ ExpressBusServices/ExpressBusServices/Patch_BusAiCorrector.cs
@@ -1,15 +1,18 @@
-﻿using HarmonyLib;
+﻿using ExpressBusServices.DataTypes;
+using ExpressBusServices.Util;
+using HarmonyLib;
 
 namespace ExpressBusServices
 {
     [HarmonyPatch(typeof(BusAI))]
-    [HarmonyPatch("CanLeave", MethodType.Normal)]
+    [HarmonyPatch(nameof(BusAI.CanLeave), MethodType.Normal)]
     public class Patch_BusAiCorrector
     {
         [HarmonyPostfix]
-        public static void PostFix(ref bool __result, ushort vehicleID, ref Vehicle vehicleData)
+        public static void ReviewDepartureStatus(ref bool __result, ushort vehicleID, ref Vehicle vehicleData)
         {
-            BusPickDropLookupTable.DetermineIfBusShouldDepart(ref __result, vehicleID, ref vehicleData);
+            VehicleDepartureUtil.ReviewDepartureStatus(ref __result, vehicleID, ref vehicleData);
+            // BusPickDropLookupTable.DetermineIfBusShouldDepart(ref __result, vehicleID, ref vehicleData);
         }
     }
 }

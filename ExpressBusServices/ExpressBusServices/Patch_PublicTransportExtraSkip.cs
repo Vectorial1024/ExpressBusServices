066527c24acd88aac3624187c4ccfd1a5854df3d
Vincent Wong <vincentwwy168@gmail.com>
2025-06-01 00:59:51 +0800

    Clean up the code

--- ExpressBusServices/ExpressBusServices/Patch_PublicTransportExtraSkip.cs
+++ ExpressBusServices/ExpressBusServices/Patch_PublicTransportExtraSkip.cs
@@ -1,9 +1,6 @@
 ﻿using HarmonyLib;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
 using JetBrains.Annotations;
 
 namespace ExpressBusServices
